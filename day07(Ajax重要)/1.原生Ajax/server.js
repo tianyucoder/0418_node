212a968ceeda86d50a3fcbb3fefb772ecae6bcfd
tianyu <0318@qq.com>
2019-07-26 16:01:42 +0800

    update

---
+++ day07(Ajax重要)/1.原生Ajax/server.js
@@ -0,0 +1,25 @@
+//1.引入express
+let express = require('express')
+
+//2.创建app服务对象
+let app = express()
+app.use(express.static('public'))
+
+
+app.get('/test_get',(request,response)=>{
+  console.log('一个GET请求来了',request.query)
+  response.send('我是服务器响应GET请求的信息')
+})
+
+app.post('/test_post',(request,response)=>{
+  console.log('一个POST请求来了')
+  response.send('我是服务器响应POST请求的信息')
+})
+
+//4.绑定端口监听
+app.listen(3000,(err)=>{
+  if (!err) {
+    console.log('测试原生js发送Ajax-GET请求的地址是：http://localhost:3000/ajax_get.html')
+  }
+  else console.log(err)
+})
