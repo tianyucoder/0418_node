0042e0581c6a7a4c7705805f165ad9c5b1ea9aa1
tianyu <0318@qq.com>
2019-07-26 16:04:54 +0800

    update

--- day07(Ajax重要)/1.原生Ajax/server.js
+++ day07(Ajax重要)/1.原生Ajax/server.js
@@ -19,6 +19,7 @@ app.post('/test_post',(request,response)=>{
 //4.绑定端口监听
 app.listen(3000,(err)=>{
   if (!err) {
+    console.log('【不要用用webstorm打开html页面，会存在跨域问题！！用以下地址】')
     console.log('测试原生js发送Ajax-GET请求的地址是：http://localhost:3000/ajax_get.html')
   }
   else console.log(err)
